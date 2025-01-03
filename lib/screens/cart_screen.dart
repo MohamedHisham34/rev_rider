9bbdc0751fd9ced1606e00b0411cdd67f4928967
mohamed321321 <mohamedhisham342000@gmail.com>
2025-01-03 22:16:31 +0200

    Cart Screen With Real Time Updates

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -1,7 +1,8 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, unnecessary_string_interpolations, prefer_const_literals_to_create_immutables
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:rev_rider/main.dart';
 
 class CartScreen extends StatefulWidget {
   const CartScreen({super.key});
@@ -11,24 +12,14 @@ class CartScreen extends StatefulWidget {
 }
 
 class _CartScreenState extends State<CartScreen> {
+  Stream<QuerySnapshot> cartItemsStream = db
+      .collection('Users')
+      .doc("${authService.currentUser!.uid}")
+      .collection('cart')
+      .snapshots();
   @override
   void initState() {
-    getData();
-    // addData();
     super.initState();
-  }
-
-  var data = [];
-  FirebaseFirestore db = FirebaseFirestore.instance;
-
-  getData() async {
-    QuerySnapshot querySnapshot = await db.collection("mohamed").get();
-    setState(() {});
-    data.addAll(querySnapshot.docs);
-  }
-
-  addData() async {
-    await db.collection("mohamed").add({"name": "ahmed", "age": "32"});
   }
 
   @override
@@ -37,15 +28,32 @@ class _CartScreenState extends State<CartScreen> {
       appBar: AppBar(
         title: const Text('Cart Page'),
       ),
-      body: SafeArea(
-        child: GridView.builder(
-          itemCount: data.length,
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2, mainAxisSpacing: 20),
-          itemBuilder: (context, index) {
-            return Text("${data[index]["name"]}");
-          },
-        ),
+      body: StreamBuilder(
+        stream: cartItemsStream,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Text("Error Getting documents");
+          }
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Container(
+              alignment: Alignment.center,
+              child: CircularProgressIndicator(),
+            );
+          }
+          return ListView.builder(
+            itemCount: snapshot.data!.docs.length,
+            itemBuilder: (context, index) {
+              return Card(
+                child: Column(
+                  children: [
+                    Text(snapshot.data?.docs[index]['itemName']),
+                    Text("${snapshot.data?.docs[index]['price'].toString()}"),
+                  ],
+                ),
+              );
+            },
+          );
+        },
       ),
     );
   }
